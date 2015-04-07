430ae32827fbc5f9b7febbbd10369f73da801cd2
AlexB <borisoft.ca@gmail.com>
2015-04-06 22:36:50 -0400

    Initial version

---
+++ src/main/java/site/BorisoftWebsiteApplication.java
@@ -0,0 +1,12 @@
+package site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorisoftWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorisoftWebsiteApplication.class, args);
+    }
+}
